984bde3e3c94b85cf99b5c6402b65c9086bab187
vini <vinicius848@gmail.com>
2020-09-20 17:37:22 -0300

    Adicao de alguns testes unitarios para o montador

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,11 @@
 #include <bits/stdc++.h>
 #include "leitordearquivos.hpp"
 #include "montador.hpp"
+#include "testes.hpp"
 
 int main(int argc, char* argv[]) {
 	montador mont1;
-	int teste;
-	printf("%d %d", mont1.getPC(), mont1.getACC());
-	mont1.insertNewSymbol("asd", 10);
-	mont1.insertNewSymbol("asd", 6);
-	teste = mont1.checkIfInstruction("ADD");
-	cout << teste << endl;
-	teste = mont1.checkIfInstruction("teste");
-	cout << teste << endl;
+	testeMontador(mont1);
+	
 	return 0;
 }
